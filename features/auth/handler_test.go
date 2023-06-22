5dbf88cd5f94021be42fba43ec75c51d8fd12242
vasapol <vasapol.rittideah@outlook.com>
2023-06-23 00:51:28 +0700

    test: implement unit test on /auth/signup route

--- features/auth/handler_test.go
+++ features/auth/handler_test.go
@@ -1,1 +1,55 @@
 package auth
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/vasapolrittideah/accord/internal/config"
+	"github.com/vasapolrittideah/accord/internal/test"
+	"github.com/vasapolrittideah/accord/models"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
+
+func TestSignUp(t *testing.T) {
+	app := fiber.New()
+	service := new(AuthServiceMock)
+
+	RegisterHandlers(app, service, &config.Config{})
+
+	signUpBody := SignUpRequest{
+		Name:            "test",
+		Email:           "test@admin.com",
+		Password:        "P@ssword!",
+		PasswordConfirm: "P@ssword!",
+	}
+
+	body, err := json.Marshal(signUpBody)
+	assert.NoError(t, err)
+
+	user := &models.User{
+		ID:        uuid.New(),
+		Name:      "Kim",
+		Email:     "kim@gmail.com",
+		Role:      "USER",
+		Provider:  "local",
+		Verified:  false,
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+	}
+
+	service.On("SignUp", signUpBody).Return(user, nil)
+
+	req := httptest.NewRequest("POST", "/auth/signup", bytes.NewBuffer(body))
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, _ := app.Test(req)
+
+	userFromResponse, err := test.GetDataFromResponse[models.User](resp)
+
+	assert.Equal(t, fiber.StatusCreated, resp.StatusCode)
+	assert.Equal(t, user, userFromResponse)
+}
--- go.mod
+++ go.mod
@@ -4,6 +4,8 @@ go 1.20
 
 require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/fatih/structs v1.1.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
@@ -27,6 +29,7 @@ require (
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
 	github.com/philhofer/fwd v1.1.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/savsgio/dictpool v0.0.0-20221023140959-7bf2e61cea94 // indirect
 	github.com/savsgio/gotils v0.0.0-20230208104028-c358bd845dee // indirect
@@ -35,6 +38,8 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/spf13/viper v1.16.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
 	github.com/tinylib/msgp v1.1.8 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
