78c2e5bd004feb6e8c980b89a297b7bdd54604d1
vasapol <vasapol.rittideah@outlook.com>
2023-06-25 11:26:10 +0700

    chore: add vendor directory to project

--- go.mod
+++ go.mod
@@ -3,33 +3,38 @@ module github.com/vasapolrittideah/accord
 go 1.20
 
 require (
+	github.com/charmbracelet/log v0.2.2
+	github.com/go-playground/locales v0.14.1
+	github.com/go-playground/universal-translator v0.18.1
+	github.com/go-playground/validator/v10 v10.14.1
+	github.com/gofiber/fiber/v2 v2.47.0
+	github.com/golang-jwt/jwt/v4 v4.5.0
+	github.com/google/uuid v1.3.0
+	github.com/matthewhartstonge/argon2 v0.3.2
+	github.com/spf13/viper v1.16.0
+	github.com/stretchr/testify v1.8.4
+	gorm.io/driver/postgres v1.5.2
+	gorm.io/gorm v1.25.1
+)
+
+require (
 	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/lipgloss v0.7.1 // indirect
-	github.com/charmbracelet/log v0.2.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/fatih/structs v1.1.0 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
 	github.com/go-logfmt/logfmt v0.6.0 // indirect
-	github.com/go-playground/locales v0.14.1 // indirect
-	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.14.1 // indirect
-	github.com/gofiber/fiber/v2 v2.47.0 // indirect
-	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
-	github.com/google/uuid v1.3.0 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
 	github.com/jackc/pgx/v5 v5.3.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/klauspost/compress v1.16.3 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
-	github.com/matthewhartstonge/argon2 v0.3.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
@@ -46,9 +51,7 @@ require (
 	github.com/spf13/cast v1.5.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.16.0 // indirect
 	github.com/stretchr/objx v0.5.0 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
 	github.com/subosito/gotenv v1.4.2 // indirect
 	github.com/tinylib/msgp v1.1.8 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
@@ -60,6 +63,4 @@ require (
 	golang.org/x/text v0.9.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	gorm.io/driver/postgres v1.5.2 // indirect
-	gorm.io/gorm v1.25.1 // indirect
 )
---
+++ vendor/github.com/matthewhartstonge/argon2/error.go
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2022. Matthew Hartstonge <matt@mykro.co.nz>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package argon2
+
+// Error represents the error code returned by argon2.
+type Error string
+
+func (e Error) Error() string {
+	return string(e)
+}
+
+var (
+	ErrOutputPtrNull         = Error("output pointer is null")
+	ErrOutputTooShort        = Error("output is too short")
+	ErrOutputTooLong         = Error("output is too long")
+	ErrPwdTooShort           = Error("password is too short")
+	ErrPwdTooLong            = Error("password is too long")
+	ErrSaltTooShort          = Error("salt is too short")
+	ErrSaltTooLong           = Error("salt is too long")
+	ErrAdTooShort            = Error("associated data is too short")
+	ErrAdTooLong             = Error("associated data is too long")
+	ErrSecretTooShort        = Error("secret is too short")
+	ErrSecretTooLong         = Error("secret is too long")
+	ErrTimeTooSmall          = Error("time cost is too small")
+	ErrTimeTooLarge          = Error("time cost is too large")
+	ErrMemoryTooLittle       = Error("memory cost is too small")
+	ErrMemoryTooMuch         = Error("memory cost is too large")
+	ErrLanesTooFew           = Error("too few lanes")
+	ErrLanesTooMany          = Error("too many lanes")
+	ErrPwdPtrMismatch        = Error("password pointer is null, but password length is not 0")
+	ErrSaltPtrMismatch       = Error("salt pointer is null, but salt length is not 0")
+	ErrSecretPtrMismatch     = Error("secret pointer is null, but secret length is not 0")
+	ErrAdPtrMismatch         = Error("associated data pointer is null, but ad length is not 0")
+	ErrMemoryAllocationError = Error("memory allocation error")
+	ErrFreeMemoryCbkNull     = Error("the free memory callback is null")
+	ErrAllocateMemoryCbkNull = Error("the allocate memory callback is null")
+	ErrIncorrectParameter    = Error("argon2_context context is null")
+	ErrIncorrectType         = Error("there is no such version of argon2")
+	ErrOutPtrMismatch        = Error("output pointer mismatch")
+	ErrThreadsTooFew         = Error("not enough threads")
+	ErrThreadsTooMany        = Error("too many threads")
+	ErrMissingArgs           = Error("missing arguments")
+	ErrEncodingFail          = Error("encoding failed")
+	ErrDecodingFail          = Error("decoding failed")
+	ErrThreadFail            = Error("threading failure")
+	ErrDecodingLengthFail    = Error("some of encoded parameters are too long or too short")
+	ErrVerifyMismatch        = Error("the password does not match the supplied hash")
+)
+
+const (
+	ARGON2_MIN_TIME = uint32(1)
+	ARGON2_MAX_TIME = uint32(4294967295)
+)
